9101aeaec0ac4147b6da4b633738d88f3fa640f8
Krzysztofga73 <119799997+Krzysztofga73@users.noreply.github.com>
2023-07-16 09:54:20 +0200

    feature(ACS): building and room service
    uzupełnienie klas service + klasy controller

--- src/main/java/com/example/accesscontrolsystem/service/RoomService.java
+++ src/main/java/com/example/accesscontrolsystem/service/RoomService.java
@@ -1,10 +1,34 @@
 package com.example.accesscontrolsystem.service;
 
-import lombok.AllArgsConstructor;
-import lombok.NoArgsConstructor;
+import com.example.accesscontrolsystem.model.RoomModel;
+import com.example.accesscontrolsystem.repository.RoomRepository;
+import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
-@Service
+import java.util.List;
 
+@Service
+@RequiredArgsConstructor
 public class RoomService {
+    private final RoomRepository roomRepository;
+
+    public void addRoom(RoomModel user) {
+        roomRepository.save(user);
+    }
+
+    public List<RoomModel> getRoomList() {
+        return roomRepository.findAll();
+    }
+
+    public RoomModel getRoomById(Long id) {
+        return roomRepository.findById(id).orElse(null);
+    }
+
+    public void saveEditRoom(RoomModel room) {
+        roomRepository.save(room);
+    }
+
+    public void removeRoom(Long id) {
+        roomRepository.deleteById(id);
+    }
 }
