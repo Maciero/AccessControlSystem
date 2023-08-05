ac8fd7d7d4a0b346436d6415f4c1d13c20f1f96a
Maciero <124499484+Maciero@users.noreply.github.com>
2023-08-05 12:25:23 +0200

    test UserModel added

--- src/test/java/com/example/accesscontrolsystem/AccessControlSystemApplicationTests.java
+++ src/test/java/com/example/accesscontrolsystem/AccessControlSystemApplicationTests.java
@@ -1,13 +1,78 @@
 package com.example.accesscontrolsystem;
 
+import com.example.accesscontrolsystem.model.*;
+import jakarta.validation.ConstraintViolation;
+import jakarta.validation.Validation;
+import jakarta.validation.Validator;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Set;
+
+import static org.assertj.core.api.Assertions.assertThat;
 
 @SpringBootTest
 class AccessControlSystemApplicationTests {
 
+    private final Validator validator = Validation.buildDefaultValidatorFactory().getValidator();
+
     @Test
-    void contextLoads() {
+    public void testValidUserModel() {
+        UserModel userModel = new UserModel();
+        userModel.setName("John");
+        userModel.setSurname("Doe");
+        userModel.setDepartment(Departments.IT);
+        userModel.setPositions(Positions.MANAGER);
+
+        List<BuildingModel> buildings = new ArrayList<>();
+        BuildingModel building = new BuildingModel();
+        buildings.add(building);
+        userModel.setBuildingModels(buildings);
+
+        List<AccessList> accessList = new ArrayList<>();
+        accessList.add(AccessList.SECURE);
+        userModel.setAccessList(accessList);
+
+        Set<ConstraintViolation<UserModel>> violations = validator.validate(userModel);
+        assertThat(violations).isEmpty();
     }
 
+    @Test
+    public void testInvalidUserModel() {
+        UserModel userModel = new UserModel();
+        userModel.setDepartment(null); // Invalid: Department is required
+
+        List<AccessList> accessList = new ArrayList<>();
+        userModel.setAccessList(accessList); // Invalid: Access List is required
+
+        List<Positions> positions = new ArrayList<>();
+        userModel.setPositions(null);
+
+        Set<ConstraintViolation<UserModel>> violations = validator.validate(userModel);
+        assertThat(violations).hasSize(3);
+    }
+
+    @Test
+    public void testValidBuildingModels() {
+        UserModel userModel = new UserModel();
+        BuildingModel building = new BuildingModel();
+        List<BuildingModel> buildings = new ArrayList<>();
+        buildings.add(building);
+        userModel.setBuildingModels(buildings);
+
+        Set<ConstraintViolation<UserModel>> violations = validator.validate(userModel);
+        assertThat(violations).isEmpty();
+    }
+
+    @Test
+    public void testInvalidBuildingModels() {
+        UserModel userModel = new UserModel();
+        List<BuildingModel> buildings = new ArrayList<>();
+        userModel.setBuildingModels(buildings); // Invalid: Building Models is empty
+
+        Set<ConstraintViolation<UserModel>> violations = validator.validate(userModel);
+        assertThat(violations).hasSize(1);
+    }
 }
